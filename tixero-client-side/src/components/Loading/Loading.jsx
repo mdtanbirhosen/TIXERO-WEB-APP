2bc1ab5fe8ed61e0aa697d777f929a38101279b1
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-03-14 11:49:57 +0600

    loding updated

--- tixero-client-side/src/components/Loading/Loading.jsx
+++ tixero-client-side/src/components/Loading/Loading.jsx
@@ -3,15 +3,15 @@ import TLogo from "../../assets/T logo.png";
 
 const Loading = () => {
   return (
-    <div className=" h-screen w-screen relative overflow-hidden bg-gray-100">
+    <div className=" h-screen w-screen relative overflow-hidden bg-[#E3E3E3] -z-50">
       {/* Logo */}
-      <div className="z-10 flex items-center justify-center h-screen">
-        <img src={TLogo} alt="Logo" className="w-32 h-32" />
+      <div className="z-50 flex items-center justify-center h-screen ">
+        <img src={TLogo} alt="Logo" className="" />
       </div>
 
       {/* Moving gradient bar */}
       <motion.div
-        className="h-screen w-10 bg-gradient-to-r from-transparent via-blue-400 to-blue-700 absolute top-0"
+        className="h-screen w-100 bg-gradient-to-r from-transparent via-red-400 to-red-500 absolute top-0 -z-10"
         initial={{ x: "-100%" }} // Start off-screen left
         animate={{ x: "100vw" }} // Move to the right end
         transition={{
